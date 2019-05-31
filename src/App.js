83668415a33c51602e163772332c64b01d4fb613
Jonathan Sorci <scottsorci@gmail.com>
2019-05-30 21:19:48 -0500

    all stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'Take out the trash',
+        completed:false
+      },
+      {
+        id:2,
+        title: 'Make Din Din',
+        completed:false
+      },
+      {
+        id:3,
+        title: 'Meeting with Boss',
+        completed:false
+      },
+    ]
+  }
+
+  markComplete = (id) => {
+    console.log(id)
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }
+    )})
+  }
+
+  render() {
+    return (
+      <div className="Main">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted'
+        }
+    }
+
+    render() {
+        const { id,title } = this.props.todo;
+        return (
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this,id)}/> {' '}
+                    { title }
+                </p>
+            </div>
+        )
+    }
+}
+
+//prop types
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem
